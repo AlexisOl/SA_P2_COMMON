9ddbf41230942e181635bed5a40e163c9b5d547c
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-10-29 10:11:58 -0600

    Create DebitoUsuario.java

---
+++ comun/src/main/java/com/example/comun/DTO/FacturaBoleto/DebitoUsuario.java
@@ -0,0 +1,18 @@
+package com.example.comun.DTO.FacturaBoleto;
+
+
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+    public class FacturaBoletoCreadoDTO {
+        private UUID userId;
+        private Double monto;
+        private String motivo;
+        private UUID funcionId;
+        private LocalDate fechaVenta;
+        private String correlationId;
+}
+
+
