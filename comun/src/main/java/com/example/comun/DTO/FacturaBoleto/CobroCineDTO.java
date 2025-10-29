e617ad8cb305ff904fe288aaeba2c316e337d51f
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-10-28 23:13:20 -0600

    Fix formatting of ventaId declaration in CobroCineDTO

--- comun/src/main/java/com/example/comun/DTO/FacturaBoleto/CobroCineDTO.java
+++ comun/src/main/java/com/example/comun/DTO/FacturaBoleto/CobroCineDTO.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 @Data
     public class CobroCineDTO {
-      private UUID ventaId,
+      private UUID ventaId;
       private UUID idCine;
       private double costo;
       private String correlationId;
