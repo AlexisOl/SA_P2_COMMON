d06f35679ccfd4556af3efd933d62733e40850d4
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-10-29 22:51:28 -0600

    Create FactruraSnackCreadaDTO.java

---
+++ comun/src/main/java/com/example/comun/DTO/FacturaBoleto/FactruraSnackCreadaDTO.java
@@ -0,0 +1,18 @@
+package com.example.comun.DTO.FacturaBoleto;
+
+
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.UUID;
+import java.util.List;
+
+@Data
+    public class FacturaBoletoCreadoDTO {
+        private UUID ventaId;
+        private UUID usuarioId;
+        private UUID idCine;
+        private UUID ventaSnackId;
+        private List<DetalleSnackDTO> detalleSnacks;
+        private String correlationId;
+}
+
