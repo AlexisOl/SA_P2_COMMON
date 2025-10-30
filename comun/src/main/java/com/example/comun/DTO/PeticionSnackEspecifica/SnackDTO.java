429a2407f197558eabc76bdb827b4ebd40485f98
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-10-30 00:27:34 -0600

    Remove 'disponible' field from SnackDTO
    Removed 'disponible' field from SnackDTO class.

---
+++ comun/src/main/java/com/example/comun/DTO/PeticionSnackEspecifica/SnackDTO.java
@@ -0,0 +1,14 @@
+package com.example.comun.DTO.PeticionSnackEspecifica;
+
+
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+public class SnackDTO {
+    private UUID id;
+    private String nombre;
+    private Double precio;
+    private String correlationId;
+}
