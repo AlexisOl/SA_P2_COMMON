d1ae985f690bc12222123fb085bb8c78792498fc
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-10-30 18:17:07 -0600

    Create DebitoCineDTO.java

---
+++ comun/src/main/java/com/example/comun/DTO/FacturaBoleto/DebitoCine/DebitoCineDTO.java
@@ -0,0 +1,13 @@
+package com.example.comun.DTO.FacturaBoleto.DebitoCine;
+
+
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+    public class DebitoCineDTO {
+        private Double monto;
+        private String correlationId;
+}
+
