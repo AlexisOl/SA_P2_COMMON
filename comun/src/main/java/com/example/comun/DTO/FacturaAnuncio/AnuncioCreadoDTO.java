ef94ecb84bc774c318a86d306db32c372d9f1def
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-11-01 23:10:56 -0600

    Add start and end date fields to AnuncioCreadoDTO

--- comun/src/main/java/com/example/comun/DTO/FacturaAnuncio/AnuncioCreadoDTO.java
+++ comun/src/main/java/com/example/comun/DTO/FacturaAnuncio/AnuncioCreadoDTO.java
@@ -4,13 +4,15 @@ package com.example.comun.DTO.FacturaAnuncio;
 
 
 import lombok.Data;
-
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
     public class AnuncioCreadoDTO {
         private UUID anuncioId;
         private UUID idCine;
+        private LocalDate fechainicio;
+        private LocalDate fechafin;
         private UUID usuarioId;
         private double costo;
         private String correlationId;
