d840aaee5b5bda97365c40ab289fc2a426523a2e
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-11-01 23:17:47 -0600

    Rename class DebitoUsuario to DebitoUsuarioAnuncio

--- comun/src/main/java/com/example/comun/DTO/FacturaAnuncio/DebitoUsuarioAnuncio.java
+++ comun/src/main/java/com/example/comun/DTO/FacturaAnuncio/DebitoUsuarioAnuncio.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
-    public class DebitoUsuario {
+    public class DebitoUsuarioAnuncio {
         private UUID userId;
         private Double monto;
         private UUID idAnuncio;
