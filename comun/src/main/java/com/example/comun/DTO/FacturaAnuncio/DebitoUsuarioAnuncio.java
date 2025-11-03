c5a0e2f2e97ab560eab77faa1156270177618c9d
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-11-02 20:20:13 -0600

    Update DebitoUsuarioAnuncio.java

--- comun/src/main/java/com/example/comun/DTO/FacturaAnuncio/DebitoUsuarioAnuncio.java
+++ comun/src/main/java/com/example/comun/DTO/FacturaAnuncio/DebitoUsuarioAnuncio.java
@@ -4,7 +4,7 @@ package com.example.comun.DTO.FacturaAnuncio;
 import lombok.Data;
 import java.time.LocalDate;
 import java.util.UUID;
-
+import java.util.List;
 @Data
     public class DebitoUsuarioAnuncio {
         private UUID userId;
@@ -12,8 +12,10 @@ import java.util.UUID;
         private UUID idAnuncio;
         private String motivo;
         private UUID factura;
+        private List<DiasDescuentoAnunciosBloqueados> dineroCines;
         private LocalDate fechainicio;
         private LocalDate fechafin;
         private String correlationId;
 }
+
 
