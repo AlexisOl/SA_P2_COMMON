08399ffd0e6451b7ba0dd3134ffd0af2a10f739b
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-11-03 13:00:04 -0600

    Update CreditoUsuarioBloqueo.java

--- comun/src/main/java/com/example/comun/DTO/BloqueoAnuncios/CreditoUsuarioBloqueo.java
+++ comun/src/main/java/com/example/comun/DTO/BloqueoAnuncios/CreditoUsuarioBloqueo.java
@@ -4,7 +4,7 @@ package com.example.comun.DTO.BloqueoAnuncios;
 import lombok.Data;
 import java.time.LocalDate;
 import java.util.UUID;
-
+import java.util.List;
 @Data
     public class CreditoUsuarioBloqueo {
         private List<CreditoUsuarioBloqueoEspecifico> listado;
