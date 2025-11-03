de3326e76241558de0161c4ac85b86bf47d841be
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-11-03 11:14:58 -0600

    Update BloqueoCineDTO.java

--- comun/src/main/java/com/example/comun/DTO/BloqueoAnuncios/BloqueoCineDTO.java
+++ comun/src/main/java/com/example/comun/DTO/BloqueoAnuncios/BloqueoCineDTO.java
@@ -4,11 +4,12 @@ package com.example.comun.DTO.BloqueoAnuncios;
 import lombok.Data;
 import java.time.LocalDate;
 import java.util.UUID;
-
+import java.util.List;
 @Data
     public class BloqueoCineDTO {
         private UUID cine;
         private LocalDate fecha;
         private Long cantidad_dias;
+        private List<PeticionBloqueoAnuncio> peticiones;
 }
 
