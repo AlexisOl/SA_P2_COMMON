6a39fc301b2e721a844101f373b8287ea2090733
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-11-07 14:04:01 -0600

    Update RespuestaFacturaBoletoCreadoDTO.java

--- comun/src/main/java/com/example/comun/DTO/FacturaBoleto/RespuestaFacturaBoletoCreadoDTO.java
+++ comun/src/main/java/com/example/comun/DTO/FacturaBoleto/RespuestaFacturaBoletoCreadoDTO.java
@@ -4,12 +4,14 @@ package com.example.comun.DTO.FacturaBoleto;
 import lombok.Data;
 import java.time.LocalDate;
 import java.util.UUID;
+import java.util.List;
 
 @Data
     public class RespuestaFacturaBoletoCreadoDTO {
       private UUID factura;
       private UUID ventaId;
       private boolean exito;
+      private List<UUID> ids;
       private String motivoFallo;
       private String correlationId;
 }
